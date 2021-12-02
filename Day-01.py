c0e7051c8128abeb5102bb1f6c4cb03ec9ee685c
Dhruv Gupta <91935003+dhruv7-sys@users.noreply.github.com>
2021-12-02 20:49:48 +0530

    Add files via upload

---
+++ Day-01.py
@@ -0,0 +1,33 @@
+# Write your code below this line
+# code 1 ( HELLO WORLD)
+print("hello world!")
+
+# code 2 (backslash:-to print code in next line)
+print("hello world\nHello world")
+
+#code 3 (To give space between the code)
+print("hello" + " " + "Dhruv")
+
+# code 4 (input function)
+print("hello" +" " + input('What is your name?'))
+
+# code 5 (Python varaibles)
+name = "Dhruv"
+print(name)
+
+print(len (input ("what is your name") ) )
+
+# code 6 ( variable naming )
+nama = "Dhruv"
+print(nama)
+
+# ***END***
+
+
+
+
+
+
+
+
+
